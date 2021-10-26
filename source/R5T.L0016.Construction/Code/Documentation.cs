285df320a8c923114e4aec1eb938bc8044cfa6d4
davidcoats <david@getsafetycone.com>
2021-10-26 11:01:05 -0700

    Initial commit of source-stub.

---
+++ source/R5T.L0016.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0016.Construction
+{
+    /// <summary>
+    /// Construction project for Microsoft.AspNetCore Nuget packages selector..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.L0016/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0016
+{
+    /// <summary>
+    /// Microsoft.AspNetCore Nuget packages selector.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
